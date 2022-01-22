7e6fccc3789d20cc49f43dba5ae2ead3e73bd596
Stacey Engstrom <kg5466or@go.minnstate.edu>
2022-01-21 19:22:30 -0600

    Add project files.

---
+++ ex1a/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ex1a.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
